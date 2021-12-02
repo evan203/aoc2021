9ca97f12b95df3c1b7c956c5e5b1de4e37766702
evan203 <evancreeper203@gmail.com>
2021-12-02 01:18:28 +0000

    prepare day 2

---
+++ day02.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class day02
+{
+	public static void main (String[] args) throws FileNotFoundException
+	{
+		File inputFile = new File("input/input02");	
+
+		// part 1:
+		Scanner input = new Scanner(inputFile);
+		while (input.hasNext())
+		{
+
+		}
+
+	}
+}
